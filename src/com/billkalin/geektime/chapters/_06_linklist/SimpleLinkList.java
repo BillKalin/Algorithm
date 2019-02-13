5249425bd4413d7b8e867907fef13496138cbdbc
BillKalin <396116131@qq.com>
2019-02-13 20:29:50 +0800

    init chapter6

---
+++ src/com/billkalin/geektime/chapters/_06_linklist/SimpleLinkList.java
@@ -0,0 +1,15 @@
+package com.billkalin.geektime.chapters._06_linklist;
+
+/**
+ * 1）单链表的插入、删除、查找操作；
+ * 2）链表中存储的是int类型的数据；
+ *
+ */
+public class SimpleLinkList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
