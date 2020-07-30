75b735c9454aa250f2c7e218a3719efc4eb483a5
BillKalin <396116131@qq.com>
2020-07-30 23:31:59 +0800

    正则表达式：dp两种算法

--- src/com/billkalin/leetcode/LeetCode10.java
+++ src/com/billkalin/leetcode/LeetCode10.java
@@ -3,7 +3,7 @@ package com.billkalin.leetcode;
 public class LeetCode10 {
     public static void main(String[] args) {
         LeetCode10 instance = new LeetCode10();
-        boolean isMatch = instance.isMatch("aab", "c*a*b");
+        boolean isMatch = instance.isMatch2("aa", ".*");
         System.out.println("isMatch= " + isMatch);
     }
 
@@ -21,5 +21,52 @@ public class LeetCode10 {
         }
     }
 
+    private int[][] mem;
 
+    //备忘录DP
+    private boolean isMatch2(String string, String pattern, int si, int pi) {
+        if (mem == null) {
+            mem = new int[string.length() + 1][pattern.length() + 1];
+        }
+        if (mem[si][pi] != 0)
+            return mem[si][pi] == 1;
+
+        boolean isMatch;
+        if (pi == pattern.length()) {
+            isMatch = si == string.length();
+        } else {
+            boolean firstMatch = si < string.length() && (string.charAt(si) == pattern.charAt(pi) || pattern.charAt(pi) == '.');
+            if (pi + 1 < pattern.length() && pattern.charAt(pi + 1) == '*') {
+                isMatch = isMatch2(string, pattern, si, pi + 2) || (firstMatch && isMatch2(string, pattern, si + 1, pi));
+            } else {
+                isMatch = firstMatch && isMatch2(string, pattern, si + 1, pi + 1);
+            }
+        }
+
+        if (isMatch) {
+            mem[si][pi] = 1;
+        } else {
+            mem[si][pi] = 2;
+        }
+        return isMatch;
+    }
+
+    // 表格法
+    private boolean isMatch2(String string, String pattern) {
+        boolean[][] dp = new boolean[string.length() + 1][pattern.length() + 1];
+        int strLength = string.length();
+        int pLength = pattern.length();
+        dp[strLength][pLength] = true;
+        for (int i = strLength; i >= 0; i--) {
+            for (int j = pLength - 1; j >= 0; j--) {
+                boolean first = i < strLength && (string.charAt(i) == pattern.charAt(j) || pattern.charAt(j) == '.');
+                if (j + 1 < pLength && pattern.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || first && dp[i + 1][j];
+                } else {
+                    dp[i][j] = first && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
 }
