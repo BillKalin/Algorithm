934c65782fdf1bca249af7d212b1b51c1feabce7
BillKalin <396116131@qq.com>
2020-08-02 20:12:29 +0800

    leet code 63: solutions

---
+++ src/com/billkalin/leetcode/LeetCode63.java
@@ -0,0 +1,59 @@
+package com.billkalin.leetcode;
+
+class LeetCode63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int i = 1; i < col; i++) {
+            if (obstacleGrid[0][i] == 0 && dp[0][i - 1] != 0) {
+                dp[0][i] = 1;
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            if (obstacleGrid[i][0] == 0 && dp[i - 1][0] != 0) {
+                dp[i][0] = 1;
+            }
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        int col = obstacleGrid[0].length;
+        int[] dp = new int[col];
+        dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int i = 1; i < col; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+
+        for (int i = 1; i < obstacleGrid.length; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                dp[0] = 0;
+            }
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else {
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+            }
+        }
+        return dp[col - 1];
+    }
+}
