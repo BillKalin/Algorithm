e613d5cf3907a97f57f73567ce3ea924ec1c2317
BillKalin <396116131@qq.com>
2020-08-12 23:19:57 +0800

    leetcode 85: solutions

---
+++ src/com/billkalin/leetcode/LeetCode85.java
@@ -0,0 +1,64 @@
+package com.billkalin.leetcode;
+
+public class LeetCode85 {
+
+    public static void main(String[] args) {
+        char[][] matrix = new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}
+        };
+
+        int max = maximalRectangle(matrix);
+        System.out.println("max = " + max);
+    }
+
+    public static int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+        int[] heights = new int[matrix[0].length];
+        int maxRect = 0;
+        for (char[] chars : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (chars[j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            maxRect = Math.max(maxRect, maxHeights(heights));
+        }
+        return maxRect;
+    }
+
+    private static int maxHeights(int[] heights) {
+        int maxArea = 0;
+        int[] lessFromLeft = new int[heights.length];
+        int[] lessFromRight = new int[heights.length];
+        lessFromLeft[0] = -1;
+        lessFromRight[heights.length - 1] = heights.length;
+
+        for (int i = 1; i < heights.length; i++) {
+            int p = i - 1;
+            while (p >= 0 && heights[p] >= heights[i]) {
+                p = lessFromLeft[p];
+            }
+            lessFromLeft[i] = p;
+        }
+
+        for (int i = heights.length - 2; i >= 0; i--) {
+            int p = i + 1;
+            while (p < heights.length && heights[p] >= heights[i]) {
+                p = lessFromRight[p];
+            }
+            lessFromRight[i] = p;
+        }
+
+        for (int i = 0; i < heights.length; i++) {
+            maxArea = Math.max(maxArea, heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1));
+        }
+
+        return maxArea;
+    }
+}
