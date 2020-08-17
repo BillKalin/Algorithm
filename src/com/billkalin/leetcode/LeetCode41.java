6173ed0f617e7734b970474b5af8a487d70659a5
BillKalin <396116131@qq.com>
2020-08-18 00:03:48 +0800

    leetcode 41

---
+++ src/com/billkalin/leetcode/LeetCode41.java
@@ -0,0 +1,34 @@
+package com.billkalin.leetcode;
+
+public class LeetCode41 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 4, -1, 1};
+        int num = firstMissingPositive(nums);
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == i + 1 || nums[i] <= 0 || nums[i] > n)
+                i++;
+            else if (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, nums[i] - 1, i);
+            } else {
+                i++;
+            }
+        }
+
+        i = 0;
+        while (i < n && nums[i] == i + 1) i++;
+        return i + 1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+}
