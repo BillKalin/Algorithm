ee71d7cb61a0f03527bed9daf17c24ea4d2078c9
BillKalin <396116131@qq.com>
2020-11-03 23:01:34 +0800

    LeetCode86

---
+++ src/com/billkalin/leetcode/LeetCode86.java
@@ -0,0 +1,23 @@
+package com.billkalin.leetcode;
+
+public class LeetCode86 {
+    public Leetcode2.ListNode partition(Leetcode2.ListNode head, int x) {
+        //哨兵模式
+        Leetcode2.ListNode beforeHead = new Leetcode2.ListNode(0), afterHeader = new Leetcode2.ListNode(0);
+        Leetcode2.ListNode before = beforeHead;
+        Leetcode2.ListNode after = afterHeader;
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;//尾插法
+                before = head;
+            } else {
+                after.next = head;
+                after = head;
+            }
+            head = head.next;
+        }
+        after.next = null;
+        before.next = afterHeader.next;
+        return beforeHead.next;
+    }
+}
